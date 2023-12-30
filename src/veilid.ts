1675c9e3a65dfacbb4d280fce2cf21962eac366e
DevMiner <devminer@devminer.xyz>
2023-12-30 21:36:51 +0100

    fix: bootstrap node not connectable because of TLS downgrade

--- src/veilid.ts
+++ src/veilid.ts
@@ -1,40 +1,40 @@
-import { VeilidConfigInner, VeilidWASMConfig } from './pkg/veilid_wasm.js';
+import { VeilidConfigInner, VeilidWASMConfig } from "./pkg/veilid_wasm.js";
 
 export const VEILID_WASM_CONFIG = {
 	logging: {
 		api: {
 			enabled: true,
-			level: 'Info'
+			level: "Info",
 		},
 		performance: {
 			enabled: true,
-			level: 'Info',
+			level: "Info",
 			logs_in_console: false,
-			logs_in_timings: true
-		}
-	}
+			logs_in_timings: true,
+		},
+	},
 } satisfies VeilidWASMConfig;
 
 export const VEILID_CORE_CONFIG = {
-	program_name: 'test',
-	namespace: 'devminer',
+	program_name: "vldchat",
+	namespace: "devminer",
 	capabilities: {
-		disable: []
+		disable: [],
 	},
 	protected_store: {
 		allow_insecure_fallback: true,
 		always_use_insecure_storage: false,
-		directory: '',
+		directory: "",
 		delete: false,
-		device_encryption_key_password: 'test'
+		device_encryption_key_password: "test",
 	},
 	block_store: {
-		directory: '',
-		delete: false
+		directory: "",
+		delete: false,
 	},
 	table_store: {
-		directory: '',
-		delete: false
+		directory: "",
+		delete: false,
 	},
 	network: {
 		connection_initial_timeout_ms: 2000,
@@ -46,16 +46,23 @@ export const VEILID_CORE_CONFIG = {
 		client_allowlist_timeout_ms: 30_000,
 		reverse_connection_receipt_time_ms: 5_000,
 		hole_punch_receipt_time_ms: 5_000,
-		network_key_password: '',
+		network_key_password: "",
 		routing_table: {
 			node_id: [],
 			node_id_secret: [],
-			bootstrap: ['ws://bootstrap.veilid.net:5150/ws'],
+			bootstrap: [
+				// "ws://vld.devminer.xyz:5150/ws",
+				"wss://vld.devminer.xyz:5150/ws",
+
+				// "ws://bootstrap.veilid.net:5150/ws",
+				// FIXME: Commented out until they have a TLS certificate
+				// "wss://bootstrap.veilid.net:5150/ws",
+			],
 			limit_over_attached: 64,
 			limit_fully_attached: 32,
 			limit_attached_strong: 16,
 			limit_attached_good: 8,
-			limit_attached_weak: 4
+			limit_attached_weak: 4,
 		},
 		rpc: {
 			concurrency: 0,
@@ -64,7 +71,7 @@ export const VEILID_CORE_CONFIG = {
 			max_timestamp_ahead_ms: 10_000,
 			timeout_ms: 5_000,
 			max_route_hop_count: 4,
-			default_route_hop_count: 4
+			default_route_hop_count: 4,
 		},
 		dht: {
 			max_find_node_count: 20,
@@ -85,7 +92,7 @@ export const VEILID_CORE_CONFIG = {
 			remote_subkey_cache_size: 1024,
 			remote_max_records: 65536,
 			remote_max_subkey_cache_memory_mb: 128,
-			remote_max_storage_space_mb: 0
+			remote_max_storage_space_mb: 0,
 		},
 		upnp: true,
 		detect_address_changes: true,
@@ -93,46 +100,46 @@ export const VEILID_CORE_CONFIG = {
 		application: {
 			http: {
 				enabled: false,
-				listen_address: ':5150',
-				path: 'app'
+				listen_address: ":5150",
+				path: "app",
 			},
 			https: {
 				enabled: false,
-				listen_address: ':5150',
-				path: 'app'
-			}
+				listen_address: ":5150",
+				path: "app",
+			},
 		},
 		tls: {
-			certificate_path: '',
-			private_key_path: '',
-			connection_initial_timeout_ms: 2_000
+			certificate_path: "",
+			private_key_path: "",
+			connection_initial_timeout_ms: 2_000,
 		},
 		protocol: {
 			udp: {
 				enabled: false,
-				listen_address: ':5150',
-				socket_pool_size: 0
+				listen_address: ":5150",
+				socket_pool_size: 0,
 			},
 			tcp: {
 				connect: false,
 				listen: false,
-				listen_address: ':5150',
-				max_connections: 0
+				listen_address: ":5150",
+				max_connections: 0,
 			},
 			ws: {
 				connect: true,
 				listen: true,
-				listen_address: ':5150',
+				listen_address: ":5150",
 				max_connections: 16,
-				path: 'ws'
+				path: "ws",
 			},
 			wss: {
 				connect: true,
 				listen: true,
-				listen_address: '',
+				listen_address: "",
 				max_connections: 16,
-				path: 'ws'
-			}
-		}
-	}
+				path: "ws",
+			},
+		},
+	},
 } satisfies VeilidConfigInner;
